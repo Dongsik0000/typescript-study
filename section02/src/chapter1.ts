c9fdac73bfb5a5352cfdd90ceaed216376b92364
Dongsik0000 <panda1842@naver.com>
2026-06-05 15:47:50 +0900

    2026-06-05

--- section02/src/chapter1.ts
+++ section02/src/chapter1.ts
@@ -7,6 +7,24 @@ let num5: number = Infinity;
 let num6: number = -Infinity;
 let num7: number = NaN;
 
+// string
+let str1: string = "hello";
+let str2: string = 'hello';
+let str3: string = `hello`;
+let str4: string = `hello ${str1}`;
+
 // boolean
 let bool1 : boolean = true;
 let bool2 : boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined 타입
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
+let boolB: false = false;
